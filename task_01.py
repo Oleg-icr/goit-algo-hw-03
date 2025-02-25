76bfe2cc926a367953638f1ceb066ae2adf699fa
Oleg <oleg.icr@gmail.com>
2025-02-25 10:55:40 +0100

    Final release 1.0

--- task_01.py
+++ task_01.py
@@ -1,10 +1,22 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 
 
-def get_days_from_today(date):
-    date = datetime.strptime(date, "%Y-%m-%d").date()
+def get_days_from_today(specified_date):
+    # current day
+    date_today = datetime.now().date()
+    # check ValueError
+    try:
+        # transform specified date to string
+        specified_date = datetime.strptime(specified_date, "%Y-%m-%d").date()
+        # calculate difference between two dates
+        difference = date_today - specified_date
+        return difference.days
+    except ValueError:
+        print("Wrong entered date or wrong format date. Correct format is YYYY-MM-DD")
 
-    return date
 
-
-print ( get_days_from_today("2025-02-03") )
+print ( get_days_from_today("2025-05-03") )
+print ( get_days_from_today("2025-01-21") )
+print ( get_days_from_today("2025.04.19") )
+print ( get_days_from_today("2025.13.32") )
+print ( get_days_from_today("2025-13-32") )
