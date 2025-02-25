1ce25aef94374dc5424b293b4df3eda4c6eeb1ef
Oleg-Icr <oleg.icr@gmail.com>
2025-02-25 21:27:41 +0100

    In progress 0.01

---
+++ task_02.py
@@ -0,0 +1,8 @@
+import random
+
+def get_numbers_ticket(min, max, quantity):
+    print (random.randrange(min, max) )
+    print(random.uniform(min, max))
+    return
+
+get_numbers_ticket(1, 20, 10)
