d62111170f013333999018cd048ceea5af3d2da5
Oleg-Icr <oleg.icr@gmail.com>
2025-02-26 21:40:07 +0100

    In progress 0.02

--- task_02.py
+++ task_02.py
@@ -1,8 +1,20 @@
 import random
 
 def get_numbers_ticket(min, max, quantity):
-    print (random.randrange(min, max) )
-    print(random.uniform(min, max))
-    return
+    random_set = set()
+    if max == quantity :
+        print (f"Result: a sequence of digits from {min} to {max}  ")
+        return random_set
+    elif quantity > max :
+        print (f"Result: a sequence of digits from {min} to {max}  ")
+        return random_set
+    elif min < 1 or max > 1000 :
+        print ("FROM should be higher or equal 1 , and TO should be lower or equal 1000")
+        return random_set
+    else:
+        while len(random_set)  < quantity:
+            random_set.add( random.randrange(min, max) )
+            # print (random.randrange(min, max) )
+        return sorted(random_set)
 
-get_numbers_ticket(1, 20, 10)
+print ( get_numbers_ticket(1, 10, 8) )
